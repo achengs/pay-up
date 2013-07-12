1c4e64e99146af7b2015b54ffdbf9570c9d7523b
Andrew Cheng <achengs@gmail.com>
2013-07-12 16:11:39 -0400

    initial commit w users groups

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PayUp::Application.config.secret_token = '4d21126a99f81c4b881ea9a533487af0e22ff453e6f850c4d90a67f2036aefa0de210d941d14be1933f3e2a59a92a3b6de9e8b8c9367478a3e7194b0c363d62c'
